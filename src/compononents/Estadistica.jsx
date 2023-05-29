cb3deed3d77e43317c48b80582a15b8a21fc577b
Santiago Espinosa Otalvaro <grua942@gmail.com>
2023-05-29 09:27:39 -0500

    fix: :bug: estadisitca
    se arreglo el problema del archivo estadistica , y ya arroja los datos correctamente

--- src/compononents/Estadistica.jsx
+++ src/compononents/Estadistica.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase/firebaseInit";
 
 export const Estadistica = () => {
-  const obtenerRegistros = () => {
-    var datos = localStorage.getItem("registroslogin");
-    console.log("Datos cargados desde LocalStorage: ", datos);
-    if (datos) {
-      return JSON.parse(datos);
-    } else {
-      return [];
-    }
-  };
+  const [arrayDatos, setArrayDatos] = useState([]);
 
-  const [registrosLogin, setRegistrosLogin] = useState(obtenerRegistros());
+	async function obtenerRegistros() {
+		const data = [];
+		const querySnapshot = await getDocs(collection(db, 'pinturas'));
+		querySnapshot.forEach((doc) => {
+			data.push({ docId: doc.id, ...doc.data() });
+		});
+
+		setArrayDatos([...arrayDatos, ...data]);  
+	}
+
+  useEffect(() => {
+		obtenerRegistros();
+	}, []);
+  // const obtenerRegistros = () => {
+  //   var datos = localStorage.getItem("registroslogin");
+  //   console.log("Datos cargados desde LocalStorage: ", datos);
+  //   if (datos) {
+  //     return JSON.parse();
+  //   } else {
+  //     return [];
+  //   }
+  // };
+
+  // const [registrosLogin, setRegistrosLogin] = useState(obtenerRegistros());
 
   function miEstadistica(opcion) {
     var i = 0;
@@ -19,18 +36,18 @@ export const Estadistica = () => {
     var miObjeto;
 
     if (opcion === 1) {
-      resultado = registrosLogin.length;
+      resultado = arrayDatos.length;
     } else if (opcion === 2) {
-      for (i = 0; i < registrosLogin.length; i++) {
-        miObjeto = registrosLogin[i];
-        resultado += parseInt(miObjeto.precio);
-      }
+      for (i = 0; i < arrayDatos.length; i++) {
+        miObjeto = arrayDatos[i];
+        resultado += parseInt(miObjeto.precio);}
+
     } else if (opcion === 3) {
-      for (i = 0; i < registrosLogin.length; i++) {
-        miObjeto = registrosLogin[i];
+      for (i = 0; i < arrayDatos.length; i++) {
+        miObjeto = arrayDatos[i];
         resultado += parseInt(miObjeto.precio);
       }
-      resultado = (resultado / registrosLogin.length).toFixed(2);
+      resultado = (resultado / arrayDatos.length).toFixed(2);
     }
     return resultado;
   }
@@ -46,7 +63,7 @@ export const Estadistica = () => {
           <div className="col">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Cantidad de Pinturas</h5>
+                <h5 className="card-title">Cantidad de Componentes</h5>
                 <p className="card-text"> {miEstadistica(1)} </p>
               </div>
             </div>
